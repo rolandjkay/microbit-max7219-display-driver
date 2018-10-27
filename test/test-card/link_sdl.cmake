745cb867335961a98cf767ec78420f6d0f11c348
Roland Kay <roland.j.kay@googlemail.com>
2018-10-27 22:08:18 +0100

    Fix deps
    Only include SDL2 on OS X

--- test/test-card/link_sdl.cmake
+++ test/test-card/link_sdl.cmake
@@ -1,1 +1,4 @@
-target_link_libraries(microbit-max7219-display-driver-test-test-card SDL2)
+
+if(${TARGET} STREQUAL "X86_OSX_NATIVE")
+  target_link_libraries(microbit-max7219-display-driver-test-test-card SDL2)
+endif()
